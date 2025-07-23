c897a9d104063e15eb9c3c7e4b6aaf8d788e64d4
AEnjoy <37976919+AEnjoy@users.noreply.github.com>
2025-07-23 15:29:54 +0800

    docs: add copyright and terms of service headers (#5)
    * ci(pylint): update workflow to run on specific branches and paths
    
    - Restrict pylint checks to 'master' and 'develop' branches
    - Limit checks to files in the 'lybic' directory with a '.py' extension
    - Add support for running on both Python 3.12 and3.13
    - Enable manual triggering of the workflow
    
    * chore: add pylintrc file with specific lint settings
    
    - Create pylintrc file to configure Pylint
    - Disable 'too-few-public-methods' and 'line-too-long' warnings
    
    * feat: update SDK import structure and add new features
    
    - Update import structure in __init__.py to include new modules
    - Add Stats module to the SDK
    - Update README.md with more detailed information about Lybic SDK
    - Update setup.py with proper encoding for long_description
    
    * docs(lybic): add copyright and terms of service headers

--- lybic/lybic.py
+++ lybic/lybic.py
@@ -1,5 +1,34 @@
-# lybic.py is the main entry point for Lybic API.
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2019-2025   Beijing Tingyu Technology Co., Ltd.
+# Copyright (c) 2025        Lybic Development Team <team@lybic.ai, lybic@tingyutech.com>
+# Copyright (c) 2025        Lu Yicheng <luyicheng@tingyutech.com>
+#
+# Author: AEnjoy <aenjoyable@163.com>
+#
+# These Terms of Service ("Terms") set forth the rules governing your access to and use of the website lybic.ai
+# ("Website"), our web applications, and other services (collectively, the "Services") provided by Beijing Tingyu
+# Technology Co., Ltd. ("Company," "we," "us," or "our"), a company registered in Haidian District, Beijing. Any
+# breach of these Terms may result in the suspension or termination of your access to the Services.
+# By accessing and using the Services and/or the Website, you represent that you are at least 18 years old,
+# acknowledge that you have read and understood these Terms, and agree to be bound by them. By using or accessing
+# the Services and/or the Website, you further represent and warrant that you have the legal capacity and authority
+# to agree to these Terms, whether as an individual or on behalf of a company. If you do not agree to all of these
+# Terms, do not access or use the Website or Services.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""lybic.py is the main entry point for Lybic API."""
+
 import os
+from sys import stderr
+
 import requests
 
 from lybic import dto
@@ -10,7 +39,8 @@ class LybicClient:
     def __init__(self,
                  org_id: str = os.getenv("LYBIC_ORG_ID"),
                  api_key: str = os.getenv("LYBIC_API_KEY"),
-                 endpoint: str = os.getenv("LYBIC_API_ENDPOINT", "https://api.lybic.cn/")):
+                 endpoint: str = os.getenv("LYBIC_API_ENDPOINT", "https://api.lybic.cn/"),
+                 timeout: int = 10):
         """
         Init lybic client with org_id, api_key and endpoint
 
@@ -24,6 +54,11 @@ class LybicClient:
 
         if endpoint.endswith("/"):
             self.endpoint = endpoint[:-1]
+
+        if timeout < 0:
+            print("Timeout cannot be negative, set to 10",file=stderr)
+            timeout = 10
+        self.timeout = timeout
 
         self.org_id = org_id
         self.endpoint = endpoint
@@ -49,7 +84,7 @@ class LybicClient:
         headers = self.headers
         if method != "POST":
             headers.pop("Content-Type")
-        response = requests.request(method, url, headers=headers, **kwargs)
+        response = requests.request(method, url, headers=headers,timeout=self.timeout, **kwargs)
         response.raise_for_status()
         return response
 
