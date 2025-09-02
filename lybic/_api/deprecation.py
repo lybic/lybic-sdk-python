22675f103c7e348c7c28266caabd13f5ed6c1c1a
luyicheng <aenjoyable@163.com>
2025-09-02 13:02:41 +0800

    refactor: update internal API modules - Add pylint skip-file comment to deprecation.py - Add docstring to internal.py

--- lybic/_api/deprecation.py
+++ lybic/_api/deprecation.py
@@ -9,7 +9,7 @@ https://github.com/matplotlib/matplotlib/blob/main/lib/matplotlib/_api/deprecati
     This module is for internal use only.  Do not use it in your own code.
     We may change the API at any time with no warning.
 """
-
+# pylint: skip-file
 import contextlib
 import functools
 import inspect
--- lybic/_api/internal.py
+++ lybic/_api/internal.py
@@ -1,3 +1,6 @@
+"""
+Internal API for Lybic.
+"""
 import inspect
 
 
